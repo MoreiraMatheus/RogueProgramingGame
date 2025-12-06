65b36dd83944d427e2603668813aa8104863943d
Matheus M. de Santana <81048603+MoreiraMatheus@users.noreply.github.com>
2025-12-05 23:02:04 -0300

    Iniciando projeto
    RPG de texto para aprender c#

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class RogueProgramingGame {
+    static void Main() {
+        string nome;
+
+        while (true) {
+            int menu;
+            Console.Write("1 - Novo Jogo\n2 - Carregar\n3 - Sair\nSelecione uma opção: ");
+            menu = int.Parse(Console.ReadLine());
+
+            if (menu == 1) {
+                Console.Write("Digite seu nome: ");
+                nome = Console.ReadLine();
+                Console.WriteLine($"Muito prazer {nome}, vamos começar nossa aventura");
+                break;
+            }
+            else if (menu == 2) {
+                Console.WriteLine("Nada por aqui ainda");
+                break;
+            }
+            else {
+                break;
+            }   
+        }
+    }
+}
