f3aca4ee0031fe0ea524dd23edceb49caaf65c77
Matheus M. de Santana <81048603+MoreiraMatheus@users.noreply.github.com>
2025-12-06 15:58:37 -0300

    novo update
    Adicionando classes selecionáveis

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
 ﻿using System;
 
+class Player {
+    public string Name;
+    public string Gender;
+    public string PlayerClass;
+    public int Hp = 10;
+    public int Str;
+    public int Def;
+    public int Spd;
+}
+
 class RogueProgramingGame {
     static void Main() {
-        string nome;
+        Player Jogador = new Player();
 
         while (true) {
             int menu;
@@ -10,9 +20,51 @@ class RogueProgramingGame {
             menu = int.Parse(Console.ReadLine());
 
             if (menu == 1) {
+                Console.Clear();
                 Console.Write("Digite seu nome: ");
-                nome = Console.ReadLine();
-                Console.WriteLine($"Muito prazer {nome}, vamos começar nossa aventura");
+                Jogador.Name = Console.ReadLine();
+
+                Console.Write("Qual seu gênero: ");
+                Jogador.Gender = Console.ReadLine();
+
+                Console.WriteLine($"Muito prazer {Jogador.Name}, vamos começar nossa aventura, mas antes escolha sua classe:\n");
+
+                int playerClassChecker;
+                while (true) {
+                    Console.Write("1 - Guerreiro\n2 - Tank\n3 - Ladino\nSua classe será: ");
+                    playerClassChecker = int.Parse(Console.ReadLine());
+
+                    if (playerClassChecker == 1) {
+                        Jogador.Hp += 2;
+                        Jogador.Str = 5;
+                        Jogador.Def = 2;
+                        Jogador.Spd = 1;
+                        Jogador.PlayerClass = "Guerreiro";
+                        break;
+                    }
+                    else if (playerClassChecker == 2) {
+                        Jogador.Hp += 2;
+                        Jogador.Str = 2;
+                        Jogador.Def = 5;
+                        Jogador.Spd = 1;
+                        Jogador.PlayerClass = "Tank";
+                        break;
+                    }
+                    else if (playerClassChecker == 3) {
+                        Jogador.Hp ++;
+                        Jogador.Str = 3;
+                        Jogador.Def = 1;
+                        Jogador.Spd = 5;
+                        Jogador.PlayerClass = "Ladino";
+                        break;
+                    }
+                    else {
+                        Console.Clear();
+                        Console.WriteLine("Escolha uma classe válida.\n");
+                    }
+                }
+
+                Console.WriteLine($"Você jogará como {Jogador.PlayerClass}, excelente escolha! Agora sim podemos dar inicio a sua jornada.");
                 break;
             }
             else if (menu == 2) {
@@ -23,5 +75,7 @@ class RogueProgramingGame {
                 break;
             }   
         }
+
+        Console.WriteLine("\nFim do programa");
     }
 }
--- obj/Debug/net8.0/RogueProgramingGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/RogueProgramingGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RogueProgramingGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65b36dd83944d427e2603668813aa8104863943d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RogueProgramingGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RogueProgramingGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
